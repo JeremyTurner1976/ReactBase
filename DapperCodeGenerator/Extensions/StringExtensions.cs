169c90338baa920aff947d39225a74e4a4ff1b44
Jeremy Turner <jeremyturner1976@eagles.ewu.edu>
2016-01-03 17:10:58 -0800

    Included Extension File to Solution

---
+++ DapperCodeGenerator/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperCodeGenerator.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Singularize(this string input)
+        {
+            return input.Substring(0, input.Length - 1);
+        }
+    }
+}
